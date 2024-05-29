cf43fc71208196b8c8838afb2aae4a6384c78edc
RB <rkrb@tyfonmail.se>
2024-05-29 23:12:22 +0200

    Wolfman.cs added

---
+++ Arv/Wolfman.cs
@@ -0,0 +1,16 @@
+﻿using ÖvningTre.Arv.Subclasses;
+using ÖvningTre.Interfaces;
+
+namespace ÖvningTre.Arv;
+
+public class Wolfman : Wolf, IPerson
+{
+    public Wolfman(string name, double weight, int age, bool canSwim) : base(name, weight, age, canSwim)
+    {
+    }
+
+    public void Talk()
+    {
+        Console.WriteLine("Jag har både ett arv och ett kontrakt.");
+    }
+}
