3fdb1efc7ea5e1511a1d9a3a24f2f64a86d27328
RB <rkrb@tyfonmail.se>
2024-05-29 22:59:41 +0200

    public setters 'n' getters for Animal.cs

--- Arv/Animal.cs
+++ Arv/Animal.cs
@@ -5,6 +5,26 @@ public abstract class Animal
     private string Name;
     private double Weight;
     private int Age;
+    
+    public string _Name
+    {
+        get => Name;
+        set => Name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public double _Weight
+    {
+        get => Weight;
+        set => Weight = value;
+    }
+
+    public int _Age
+    {
+        get => Age;
+        set => Age = value;
+    }
+
+    
 
     public abstract void DoSound();
     
